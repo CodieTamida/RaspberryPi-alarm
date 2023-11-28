ae7622f09f041f58040fd03f502e39093c3275dc
CodieTamida <codietamida@gmail.com>
2023-11-27 19:02:44 -0800

    Refactored code to detect multiple quick button presses

--- button.py
+++ button.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 import pygame
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 pygame.init()
@@ -22,70 +22,60 @@ GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 button_press_count= 0
 last_press = None
-blinking = False
 alarm_on = True
+
 
 def button_press(channel):
 	global button_press_count, last_press, alarm_on
-	current_time = datetime.now()
 	
-	if last_press and (current_time - last_press).total_seconds() < 5:
+	if last_press and (datetime.now() - last_press).total_seconds() < 4:
 		button_press_count += 1
 	else:
 		button_press_count =  1
 		
-	last_press = current_time
+	last_press = datetime.now()
 	
+	print("this is count before sleep: ", button_press_count)
+	print(button_press_count)
 	if button_press_count == 1:
+		print(pygame.mixer.music.get_busy())
 		if pygame.mixer.music.get_busy():
+			print(pygame.mixer.music.get_busy())
 			pygame.mixer.music.stop()
-			alarm_on = False
-			print("Alarm is Snoozed")
+			print("Alarm is Stopped, detecting Snoze or Off")
 			time.sleep(15)
-			alarm_on = True
-			pygame.mixer.music.play()
-	elif button_press_count >= 2:
+			if alarm_on:
+				pygame.mixer.music.play()
+
+	elif button_press_count > 1:
 		pygame.mixer.music.stop()
 		alarm_on = False
 		print("Alarm is Stopped")
 
-def button_callback(channel):
-	global blinking
-	blinking = not blinking
-	if pygame.mixer.music.get_busy():
-		pygame.mixer.music.stop()
-	else:
-		pygame.mixer.music.play()
-	
-GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_press, bouncetime=200)
+	print("does the function end")
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_press, bouncetime=500)
 
 def alarm_start_time(scheduled_time):
 	global alarm_on
 	while True:
 		current_time = datetime.now()
-		if current_time.hour == scheduled_time.hour and current_time.minute == scheduled_time.minute:
-			if not pygame.mixer.music.get_busy() and alarm_on:
-				pygame.mixer.music.play()
+		if current_time.hour == scheduled_time.hour and current_time.minute == scheduled_time.minute and alarm_on:	
+			pygame.mixer.music.play()
 			break
 		time.sleep(1)
 		
-scheduled_time = datetime.now().replace(hour =17, minute = 25, second = 0, microsecond = 0)
+scheduled_time = datetime.now().replace(hour =18, minute = 59, second = 20, microsecond = 0)
 
-	
+def blinking_light():
+	while True:
+		GPIO.output(yellowLed, GPIO.HIGH)
+		time.sleep(1)
+		GPIO.output(yellowLed, GPIO.LOW)
+		time.sleep(1)
 
 try:
 	alarm_start_time(scheduled_time)
-	while True:
-		if blinking:
-			print('press')
-			GPIO.output(yellowLed, GPIO.HIGH)
-			sleep(1)
-			GPIO.output(yellowLed, GPIO.LOW)
-			sleep(1)
-
-		else:
-			GPIO.output(yellowLed,GPIO.LOW)
-			sleep(.1)
+	blinking_light()
 except KeyboardInterrupt:
 			
 	GPIO.cleanup()
