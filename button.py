e530ed8c86c7a9528f614f6e17791c7e0f4f0f74
CodieTamida <codietamida@gmail.com>
2023-11-27 17:26:19 -0800

    Alarm starts at certain time

--- button.py
+++ button.py
@@ -2,6 +2,9 @@ import RPi.GPIO as GPIO
 from time import sleep
 import pygame
 import time
+from datetime import datetime, timedelta
+
+
 pygame.init()
 pygame.mixer.init()
 
@@ -17,7 +20,34 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(yellowLed, GPIO.OUT)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+button_press_count= 0
+last_press = None
 blinking = False
+alarm_on = True
+
+def button_press(channel):
+	global button_press_count, last_press, alarm_on
+	current_time = datetime.now()
+	
+	if last_press and (current_time - last_press).total_seconds() < 5:
+		button_press_count += 1
+	else:
+		button_press_count =  1
+		
+	last_press = current_time
+	
+	if button_press_count == 1:
+		if pygame.mixer.music.get_busy():
+			pygame.mixer.music.stop()
+			alarm_on = False
+			print("Alarm is Snoozed")
+			time.sleep(15)
+			alarm_on = True
+			pygame.mixer.music.play()
+	elif button_press_count >= 2:
+		pygame.mixer.music.stop()
+		alarm_on = False
+		print("Alarm is Stopped")
 
 def button_callback(channel):
 	global blinking
@@ -27,9 +57,24 @@ def button_callback(channel):
 	else:
 		pygame.mixer.music.play()
 	
-GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=300)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_press, bouncetime=200)
+
+def alarm_start_time(scheduled_time):
+	global alarm_on
+	while True:
+		current_time = datetime.now()
+		if current_time.hour == scheduled_time.hour and current_time.minute == scheduled_time.minute:
+			if not pygame.mixer.music.get_busy() and alarm_on:
+				pygame.mixer.music.play()
+			break
+		time.sleep(1)
+		
+scheduled_time = datetime.now().replace(hour =17, minute = 25, second = 0, microsecond = 0)
+
+	
 
 try:
+	alarm_start_time(scheduled_time)
 	while True:
 		if blinking:
 			print('press')
